4d4c83f81582d191dda788e36522fa9e8cf93746
Patryk <musial.patryk10@gmail.com>
2020-09-23 21:53:17 +0200

    Add login page layout and directory structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddProductPageComponent } from './pages/add-product-page/add-product-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'panel-logowania',
+    component: LoginPageComponent
+  },
+  {
+    path: 'dodaj-produkt',
+    component: AddProductPageComponent
+  },
+  {
+    path: '',
+    component: ProductListPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
+import { AddProductPageComponent } from './pages/add-product-page/add-product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    ProductListPageComponent,
+    AddProductPageComponent
   ],
   imports: [
     BrowserModule,
